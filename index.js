f758ae39d579acb9be9d82a16e3005e2dc17e97e
Jaruba Firanelli <branza.alex@gmail.com>
2019-02-25 09:03:34 +0200

    Beam Me Up

---
+++ index.js
@@ -0,0 +1,30 @@
+var path = require('path')
+var fs = require('fs')
+var child = require('child_process')
+
+var back = '../..';
+var binDir = path.resolve(__dirname, back, 'bin');
+
+// in case of asar, go one level down
+while (binDir.includes('/app.asar/') || binDir.includes('\\app.asar\\')) {
+    back += '/..'
+    binDir = path.resolve(__dirname, back, 'bin');
+}
+
+// for safety
+if (!fs.existsSync(binDir)) {
+  back += '/..'
+  binDir = path.resolve(__dirname, back, 'bin');
+}
+
+var torrentThread = path.join(binDir, 'torrent-thread')
+
+var fileName = 'torrent-thread' + (process.platform == 'win32' ? '.exe' : '')
+
+if (fs.existsSync(path.join(torrentThread, fileName)))
+  torrentThread = path.join(torrentThread, fileName)
+
+module.exports = function(port) {
+  child.exec('"' + torrentThread + '" --powPort=' + port);
+  return child
+}
---
+++ install.js
@@ -0,0 +1,44 @@
+var request = require('request')
+var fs = require('fs')
+var path = require('path');
+
+var user = 'jaruba'
+var tag = '0.0.1'
+var repoName = 'torrent-thread'
+
+let platform
+
+if (process.platform == 'darwin')
+  platform = 'macos'
+else if (process.platform == 'win32')
+  platform = 'win'
+else
+  platform = 'linux'
+
+let arch
+
+if (['ia32', 'x32'].indexOf(process.arch) > -1)
+  arch = 'x32'
+else
+  arch = 'x64'
+
+var pkg = 'torrent-thread-' + platform + '-' + arch + (platform == 'win' ? '.exe' : '')
+var url = 'https://github.com/' + user + '/' + repoName + '/releases/download/' + tag + '/' + pkg
+var binDir = path.resolve(__dirname, '../..', 'bin');
+
+if (!fs.existsSync(binDir))
+  fs.mkdirSync(binDir);
+
+request
+  .get(url)
+  .on('error', function (err) {
+    throw err
+  })
+  .pipe(fs.createWriteStream(pkg))
+  .on('close', function () {
+
+    if (!fs.existsSync(path.join(binDir, 'torrent-thread')))
+      fs.mkdirSync(path.join(binDir, 'torrent-thread'));
+
+    fs.rename(pkg, path.join(binDir, 'torrent-thread', 'torrent-thread' + (platform == 'win' ? '.exe' : '')));
+  })
